1c9cde6ff348c5fa7ab81b6c80216e0bf19e727d
pramindanata <pramindanata.eksa@gmail.com>
2020-07-29 19:14:33 +0800

    signout done

--- auth/src/routes/signout.ts
+++ auth/src/routes/signout.ts
@@ -1,8 +1,11 @@
 import { Router } from 'express'
+import type { Request, Response } from 'express'
 
 const router = Router()
 
-router.post('/signout', (req, res) => {
+router.post('/signout', (req: Request, res: Response) => {
+  req.session = null
+
   return res.send('ok')
 })
 
