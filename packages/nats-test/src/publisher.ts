9cd292006620d695952ce10294c08ac59089dee4
Eksa Pramindanata <pramindanata.eksa@gmail.com>
2020-08-11 15:22:00 +0800

    [nats-test] add nats test project

--- packages/nats-test/src/publisher.ts
+++ packages/nats-test/src/publisher.ts
@@ -1,1 +1,23 @@
-console.log('pub')
+import nodeStan from 'node-nats-streaming'
+
+console.clear()
+
+const stan = nodeStan.connect('git-tix', 'abc', {
+  url: 'http://localhost:4222',
+})
+
+stan.on('connect', () => {
+  console.log('publisher is connected')
+
+  const data = JSON.stringify({
+    id: 1,
+    title: 'sport event',
+    price: 20,
+  })
+
+  setInterval(() => {
+    stan.publish('ticket:created', data, () => {
+      console.log('message published')
+    })
+  }, 1500)
+})
--- packages/nats-test/src/subscriber.ts
+++ packages/nats-test/src/subscriber.ts
@@ -1,1 +1,43 @@
-console.log('suib')
+import nodeStan, { Message } from 'node-nats-streaming'
+import { randomBytes } from 'crypto'
+
+console.clear()
+
+const clientId = randomBytes(4).toString('hex')
+const stan = nodeStan.connect('git-tix', 'some-client-id3', {
+  url: 'http://localhost:4222',
+})
+const getStanData = <Result = any>(msg: Message): Result => {
+  const data = msg.getData()
+
+  return JSON.parse(data.toString())
+}
+
+stan.on('connect', () => {
+  console.log('subscriber is connected')
+
+  stan.on('close', () => {
+    console.log('STAN connection closed')
+    process.exit()
+  })
+
+  const options = stan
+    .subscriptionOptions()
+    .setManualAckMode(true)
+    .setDeliverAllAvailable()
+    .setDurableName('nats-test')
+
+  const ticketCreatedSubscriber = stan.subscribe('ticket:created', options)
+
+  ticketCreatedSubscriber.on('message', (msg: Message) => {
+    const data = getStanData(msg)
+    const sequence = msg.getSequence()
+
+    console.log(`Received message #${sequence}:`, data)
+
+    msg.ack()
+  })
+})
+
+process.on('SIGINT', () => stan.close())
+process.on('SIGTERM', () => stan.close())
