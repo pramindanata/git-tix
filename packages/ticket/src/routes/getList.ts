c6d87ff83ced02737a3ec9ea16dad61506a270ff
Eksa Pramindanata <pramindanata.eksa@gmail.com>
2020-08-27 14:44:11 +0800

    [ticket] update getList to return non ordered data

--- packages/ticket/src/routes/getList.ts
+++ packages/ticket/src/routes/getList.ts
@@ -8,7 +8,9 @@ import { RO } from '../interface'
 const router = Router()
 
 router.get('/', async (req, res: Response<RO.List<TicketDTO>>) => {
-  const tickets = await Ticket.find()
+  const tickets = await Ticket.find({
+    orderId: undefined,
+  })
 
   return res.json({
     data: tickets.map((ticket) => new TicketDTO(ticket)),
