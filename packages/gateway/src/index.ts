c2297f96fdda3627881982db397e800e3da36048
Eksa Pramindanata <pramindanata.eksa@gmail.com>
2020-09-02 15:07:53 +0800

    [gateway] add morgan

--- packages/gateway/src/index.ts
+++ packages/gateway/src/index.ts
@@ -4,19 +4,23 @@ dotenv.config()
 
 import gateway from 'fast-gateway'
 import cookieParser from 'cookie-parser'
+import morgan from 'morgan'
 import { config } from './config'
 import { authRoutes, ticketRoutes } from './routes'
 
 const { app: appConfig } = config
+const logger = morgan(
+  ':method :url :status :res[content-length] - :response-time ms',
+)
 
 const server = gateway({
-  middlewares: [cookieParser()],
+  middlewares: [logger, cookieParser()],
   routes: [...authRoutes, ...ticketRoutes],
 })
 
-// eslint-disable-next-line no-console
-console.log(
-  `${appConfig.name} is listening on http://localhost:${appConfig.port}`,
-)
-
-server.start(appConfig.port)
+server.start(appConfig.port).then(() => {
+  // eslint-disable-next-line no-console
+  console.log(
+    `${appConfig.name} is listening on http://localhost:${appConfig.port}`,
+  )
+})
